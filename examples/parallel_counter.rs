98869cc320f9af6eff3b4cb1e176844100a77406
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-21 22:52:26 +0100

    Add an example that should count the number of 0s in a vec
    This deadlocks right now, maybe due to a bug in desync?

---
+++ examples/parallel_counter.rs
@@ -0,0 +1,71 @@
+extern crate desync;
+extern crate flo_stream;
+extern crate futures;
+extern crate rand;
+
+use desync::*;
+use flo_stream::*;
+use futures::*;
+use futures::stream;
+use futures::executor;
+
+use std::sync::*;
+
+fn main() {
+    // Demonstrates how the single publisher can be used with desync to schedule work across multiple threads
+
+    // Task that takes some chunks of work (vectors of numbers) and counts the number of 0s in each, returning a stream of results
+    fn count_zeros<In: 'static+Send+Stream<Item=Vec<u32>, Error=()>>(input: In) -> impl Stream<Item=u32, Error=()> {
+        // There's no state, so we desync around a void type
+        let worker = Arc::new(Desync::new(()));
+
+        // Counts the number of 0s in the input vector, asynchronously
+        pipe(worker, input, |_state, next| {
+            let mut count = 0;
+            
+            if let Ok(next) = next {
+                for val in next {
+                    if val == 0 {
+                        count += 1;
+                    }
+                }
+            }
+
+            Ok(count)
+        })
+    }
+
+    // Buffer size of 1 means that this will generate back pressure when any worker is busy
+    let mut work_publisher = SinglePublisher::new(1);
+
+    // Create 5 workers to receive work from the publisher
+    let workers = (0..5).into_iter()
+        .map(|_| count_zeros(work_publisher.subscribe()))
+        .collect::<Vec<_>>();
+
+    // Input stream is 100,000,000 random numbers
+    let input_stream = stream::iter_ok::<_, ()>((0..100_000_000)
+        .into_iter()
+        .map(|_| rand::random::<u32>() % 1024));
+    
+    // Slice into chunks with 32k numbers each
+    let input_work = input_stream.chunks(32000);
+
+    // Send the chunks to the work publisher to be scheduled
+    let work_done = work_publisher.send_all(input_work);
+
+    // Count up the results in another desync object
+    let final_count = Arc::new(Desync::new(0));
+    workers.into_iter().for_each(|worker| {
+        pipe_in(final_count.clone(), worker, |state, next| {
+            *state += next.unwrap_or(0);
+            println!("So far: {}", *state);
+        });
+    });
+
+    // Wait for the processing to finish
+    executor::spawn(work_done).wait_future().unwrap();
+
+    // Notify about the final count when we're done
+    final_count.sync(|count| println!("Final count was {}", count));
+}
